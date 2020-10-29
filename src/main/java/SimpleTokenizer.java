bc3613afa12f3efbf5498c67e80d0c13df965cf8
Luís Fonseca <39202314+luis99fonseca@users.noreply.github.com>
2020-10-29 02:05:53 +0000

    Update SimpleTokenizer.java

--- src/main/java/SimpleTokenizer.java
+++ src/main/java/SimpleTokenizer.java
@@ -7,7 +7,7 @@ public class SimpleTokenizer implements Tokenizer {
         return corpus.replaceAll("[^a-zA-Z]", " ")      // remove non-alphabetic chars
                 .replaceAll("\\b\\w{1,2}\\b\\s?", "")   // remove words less 2 chars
                 .trim()
-                .toLowerCase()                                           // lowercase
-                .split("\\s+");                                    // split whitespaces
+                .toLowerCase()                          // lowercase
+                .split("\\s+");                         // split whitespaces
     }
 }
