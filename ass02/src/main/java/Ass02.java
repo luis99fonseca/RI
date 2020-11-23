d87ddaca42f55031f7fcfd364cfc91e264b52c28
LUISCDF <luiscdf@ua.pt>
2020-11-23 17:43:20 +0000

    begin results gather

--- ass02/src/main/java/Ass02.java
+++ ass02/src/main/java/Ass02.java
@@ -61,57 +61,106 @@ public class Ass02 {
         Searcher s = new Searcher("results.txt", tokenizer);
         ////Map<String, List<Post>> inverted_index = s.getInverted_indexer();
 
-//        Map<String, Double> scores =  s.searchingBM25("coronavirus origin");
-        Map<String, Double> scores =  s.searchingLncLtc("coronavirus origin");
+////        Map<String, Double> scores =  s.searchingBM25("coronavirus origin");
+//        Map<String, Double> scores =  s.searchingLncLtc("coronavirus origin");
 
-        //
-        System.out.println("Search: coronavirus origin");
-        for(String key :scores.keySet())
-            System.out.println(key+" -> " + scores.get(key));
-//
-        System.out.println("> " + scores.size());
-
-
+///////////////////////////////////////7
         File my_file = new File("data/queries.relevance.filtered.txt");
         Scanner myReader = new Scanner(my_file);
 
         Map<String, List<String>> queries_solutions = new HashMap<>();
 
-        while (myReader.hasNextLine()){
+        while (myReader.hasNextLine()) {
             String[] line = myReader.nextLine().split(" ");
             queries_solutions.putIfAbsent(line[0], new ArrayList<>());
             queries_solutions.get(line[0]).add(line[1]);
         }
 
-        System.out.println("queries size: " + queries_solutions.size());
-        System.out.println(queries_solutions.get("1").size());
+        System.out.println("SOLUTIONS SIZE= " + queries_solutions.size());
+        System.out.println("SOLUTIONS (1) SIZE= " + queries_solutions.get("1").size());
 
-//        for(int i = 0; i < queries_solutions.get("1").size(); i++){
-//            if (scores.get(queries_solutions.get("1").get(i)) == null){
-//                System.out.println("F - " + queries_solutions.get("1").get(i));
-//            } else {
-//                System.out.println("R - " + queries_solutions.get("1").get(i));
-//            }
-//        }
+        File queries_file = new File("data/queries.txt");
+        Scanner queries_reader = new Scanner(queries_file);
 
-        int top_count = 0;
-        int true_positives = 0;
+        int top_count ;
+        int true_positives;
         List<Double> precision_list = new ArrayList<>();
+        double sum_precision ;
         List<Double> recall_list = new ArrayList<>();
-        for(String key :scores.keySet()){
-            top_count++;
-            if (queries_solutions.get("1").contains(key)){
-                true_positives++;
-                precision_list.add( ( (double) true_positives / top_count));
-                recall_list.add(( (double) true_positives / queries_solutions.size()));
+        double sum_recall ;
+        List<Double> f1_score = new ArrayList<>();
+
+        Map<String, ResultsInformation> results_rank10 = new HashMap<>();
+        Map<String, ResultsInformation> results_rank20 = new HashMap<>();
+        Map<String, ResultsInformation> results_rank50 = new HashMap<>();
+
+//        double query_throughput = 0.0;
+        int lines_read = 50; // TODO: ser mais dinamico??
+        double query_throughput[] = new double[lines_read];
+        for (int l = 1; l <= lines_read; l++) {
+            top_count = 0;
+            true_positives = 0;
+            sum_precision = 0.0;
+            sum_recall = 0.0;
+
+            String query = queries_reader.nextLine();
+            final long startTime = System.nanoTime();
+            Map<String, Double> scores = s.searchingLncLtc(query);
+            query_throughput[l - 1] = (System.nanoTime() - startTime) / (Math.pow(10, 9));
+            System.out.println("QUERY= " + query + "; RESULTS= " + scores.size());
+
+            for (String key : scores.keySet()) {
+                top_count++;
+                if (queries_solutions.get(l + "").contains(key)) {
+                    true_positives++;
+//                precision_list.add( ( (double) true_positives / top_count));
+                    sum_precision += ((double) true_positives / top_count);
+//                recall_list.add(( (double) true_positives / queries_solutions.get("1").size()));
+                    sum_recall += ((double) true_positives / queries_solutions.get("1").size());
+                }
+                if (top_count == 10) {
+//                precision_list.add( sum_precision / true_positives );
+//                recall_list.add( sum_recall / true_positives );
+//                f1_score.add(  (2 * precision_list.get(precision_list.size()-1) * recall_list.get(recall_list.size()-1))
+//                        / (precision_list.get(precision_list.size()-1) + recall_list.get(recall_list.size()-1))
+//                );
+                    results_rank10.put(l + "", new ResultsInformation(sum_precision / true_positives, sum_recall / true_positives));
+                }
+                if (top_count == 20) {
+//                precision_list.add( sum_precision / true_positives );
+//                recall_list.add( sum_recall / true_positives );
+//                f1_score.add(  (2 * precision_list.get(precision_list.size()-1) * recall_list.get(recall_list.size()-1))
+//                        / (precision_list.get(precision_list.size()-1) + recall_list.get(recall_list.size()-1))
+//                );
+                    results_rank20.put(l + "", new ResultsInformation(sum_precision / true_positives, sum_recall / true_positives));
+                }
+                if (top_count == 50) {
+//                precision_list.add( sum_precision / true_positives );
+//                recall_list.add( sum_recall / true_positives );
+//                f1_score.add(  (2 * precision_list.get(precision_list.size()-1) * recall_list.get(recall_list.size()-1))
+//                        / (precision_list.get(precision_list.size()-1) + recall_list.get(recall_list.size()-1))
+//                );
+                    results_rank50.put(l + "", new ResultsInformation(sum_precision / true_positives, sum_recall / true_positives));
+                    break;
+                }
             }
         }
-        for (int i = 0; i < precision_list.size(); i++) {
-            System.out.println("P= " + precision_list.get(i));
-            System.out.println("R= " + recall_list.get(i));
+        System.out.println("Query Throughput= " + (Arrays.stream(query_throughput).sum() / lines_read) + "; " + (lines_read / Arrays.stream(query_throughput).sum()));
+        Arrays.sort(query_throughput);
+        double median;
+        if (query_throughput.length % 2 == 0)
+            median = (query_throughput[query_throughput.length/2] + (double)query_throughput[query_throughput.length/2 - 1])/2;
+        else
+            median = query_throughput[query_throughput.length/2];
+//        double median = ((double) query_throughput[query_throughput.length / 2] + (double) query_throughput[query_throughput.length / 2 - 1]) / 2;
+        System.out.println("Query Latency (Median in Seconds)= " + median);
+
+        for (int i = 1; i <= lines_read; i++) {
+            System.out.println(results_rank10.get(i + ""));
+            System.out.println(results_rank20.get(i + ""));
+            System.out.println(results_rank50.get(i + ""));
+            System.out.println("");
         }
-        System.out.println(top_count);
-        System.out.println(true_positives);
 
     }
 }
